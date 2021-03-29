38a7a6043a72a84e39964c238eadf4453e2e8a55
rodelcabs <cabubas27rodel@gmail.com>
2021-03-29 09:41:31 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+from textblob import TextBlob
+import nltk
+from nltk.tokenize import sent_tokenize
+import json
+import requests
+import urllib.parse
+
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('text', type=str)
+
+class Translator(Resource):
+    def get(self):
+        text = parser.parse_args()
+        tl_blob = TextBlob(text["text"])
+        
+        try:
+            translated = tl_blob.translate(from_lang='tl', to='en')
+        except:
+            translated = text["text"]
+        
+        # return json.loads(json.dumps(str(translated)))
+        tag=str(translated)
+        
+        tokens=nltk.word_tokenize(tag)
+        pos_tag = nltk.pos_tag(tokens)
+
+        # kuha lang ng adjective, proper noun, adverb, verb 
+        tags = [
+            'JJ',
+            # 'JJR',
+            # 'JJS',
+            'VB',
+            # 'VBG',
+            # 'VBD',
+            # 'VBN',
+            'VBP',
+            # 'VBZ',
+            'NN',
+            'NNS'
+        ]
+
+        # final words
+        final = []
+
+        for i in pos_tag:
+            if i[1] in tags and i[0] != "is" and len(i[0]) > 2:
+                final.append(i[0].strip())
+
+        return json.loads(json.dumps(final))
+        
+api.add_resource(Translator, '/translate')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
